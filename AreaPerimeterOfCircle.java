2d914c01fde646cce1fe4c68dd12c774125e6a9b
Dorant Dushi <75392306+Dori66@users.noreply.github.com>
2021-11-12 16:21:20 +0100

    Add files via upload

---
+++ AreaPerimeterOfCircle.java
@@ -0,0 +1,6 @@
+public class AreaPerimeterOfCircle {
+    public static void main(String[] args) {
+        System.out.println("Perimeter: "+ 2 * 3.14 * 5.5);
+        System.out.println("Area: "+5.5 * 5.5 * 3.14);
+    }
+}
